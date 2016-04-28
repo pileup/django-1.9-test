73534eafb495865eb10af8ae1e4b05c2d68dfa80
pileup <pileup@gmail.com>
2016-04-28 16:34:40 -0400

    adding urls

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,4 @@ from .models import Post, Author, Category, Tag
 admin.site.register(Post)
 admin.site.register(Author)
 admin.site.register(Tag)
-admin.site.register(Category)
+admin.site.register(Category) 
